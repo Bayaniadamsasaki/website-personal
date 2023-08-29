5a09cc0c636b349a86e9c8156a5c66a1a7669e8e
Bayaniadamsasaki <bayaniadamsasaki@gmail.com>
2023-08-29 14:54:51 +0700

    Update Deskripsi

--- script/script.js
+++ script/script.js
@@ -1,8 +1,30 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+  slidesPerView: getInitialSlidesPerView(), 
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+
+function getInitialSlidesPerView() {
+  if (window.innerWidth > 768) {
+    return 3; // Laptop width
+  } else {
+    return 1; // Mobile width
+  }
+}
+
+window.addEventListener("resize", function () {
+  swiper.params.slidesPerView = getUpdatedSlidesPerView();
+  swiper.update();
+});
+
+function getUpdatedSlidesPerView() {
+  if (window.innerWidth > 768) {
+    return 3; // Laptop width
+  } else {
+    return 1; // Mobile width
+  }
+}
